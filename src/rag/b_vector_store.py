68de50a4c9b11ee7db4b7418782ed3c92fee8842
Danial Zulkifli <danial.zulkifli@nxp.com>
2026-03-26 03:35:49 +0800

    .

--- src/rag/b_vector_store.py
+++ src/rag/b_vector_store.py
@@ -1,27 +1,23 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
 import ssl
-from langchain_community.vectorstores.lancedb import LanceDB 
 import os
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores.lancedb import LanceDB
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-def load_embedding_model():
 
-    # embedding_model = OllamaEmbeddings(model="nomic-embed-text")
-    embedding_model = HuggingFaceEmbeddings(
+def load_embedding_model():
+    return HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2",
         model_kwargs={"token": os.getenv("HF_TOKEN")}
     )
 
-    return embedding_model
 
 def load_vector_store():
-
-    embedding_model = load_embedding_model()
+    embedding = load_embedding_model()
 
     vectorstore = LanceDB(
         uri="vector_store",
-        embedding=embedding_model
+        embedding=embedding
     )
-
     return vectorstore
