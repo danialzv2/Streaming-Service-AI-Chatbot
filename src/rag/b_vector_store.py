b44ed292b6408c2fc4cd3a0a4b622420c8f5bec5
Danial Zulkifli <danial.zulkifli@nxp.com>
2026-03-26 09:14:06 +0800

    .

--- src/rag/b_vector_store.py
+++ src/rag/b_vector_store.py
@@ -1,23 +1,25 @@
 import ssl
 import os
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores.lancedb import LanceDB
+from langchain_community.vectorstores import FAISS
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
 def load_embedding_model():
     return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2",
-        model_kwargs={"token": os.getenv("HF_TOKEN")}
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
 
 def load_vector_store():
+
     embedding_model = load_embedding_model()
 
-    vectorstore = LanceDB(
-        uri="../Tonton RAG Chatbot/vector_store",
-        embedding=embedding_model
+    vectorstore = FAISS.load_local(
+        folder_path="../Tonton RAG Chatbot/vector_store",
+        embeddings=embedding_model,
+        allow_dangerous_deserialization=True
     )
     return vectorstore
+
